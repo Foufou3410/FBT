7f2373c636cc78b70ad381c9a3d0d2f61ec8139a
ensimag <ensimag@ENSIPC438>
2017-09-05 14:24:39 +0200

    First PayOff

--- WpfApplication1/WpfApplication1/MainWindowView.cs
+++ WpfApplication1/WpfApplication1/MainWindowView.cs
@@ -11,6 +11,13 @@ namespace Fbt
         public MainWindowView()
         {
             Console.WriteLine("Demarrer");
+            var date = new DateTime(24/06/2018);
+            var sh = new PricingLibrary.FinancialProducts.Share("BNP", "1");
+            //var sh2 = new PricingLibrary.FinancialProducts.Share("CA", "2");
+            var opt = new PricingLibrary.FinancialProducts.VanillaCall("option", sh, date, 12);
+            var dic = new Dictionary<String, decimal>() { {"1", 38.1m }};
+            var payoff = opt.GetPayoff(dic);
+            Console.WriteLine(payoff);
         }
     }
 }
