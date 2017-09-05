578dbb36783aa58a314972d71ce83a02187ed30c
ensimag <ensimag@ENSIPC438>
2017-09-05 13:15:27 +0200

    Creation of a new project

---
+++ WpfApplication1/WpfApplication1/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Fbt
+{
+    class Program
+    {
+        static void Test(string[] args)
+        {
+            Console.WriteLine("coucou");
+        }
+    }
+}
