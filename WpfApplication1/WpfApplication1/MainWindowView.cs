4f1bf6cede51ee1f46e9af2b669ab9f8a8af4785
ensimag <ensimag@ENSIPC432>
2017-09-05 15:37:13 +0200

    Interface Version 1

--- WpfApplication1/WpfApplication1/MainWindowView.cs
+++ WpfApplication1/WpfApplication1/MainWindowView.cs
@@ -8,16 +8,33 @@ namespace Fbt
 {
     class MainWindowView
     {
+        private DateTime date;
+        private PricingLibrary.FinancialProducts.Share sh;
+        private PricingLibrary.FinancialProducts.VanillaCall opt;
+        private Dictionary<String, decimal> dic;
+
+        #region Public Constructors
+
         public MainWindowView()
         {
             Console.WriteLine("Demarrer");
-            var date = new DateTime(24/06/2018);
-            var sh = new PricingLibrary.FinancialProducts.Share("BNP", "1");
+            date = new DateTime(24/06/2018);
+            sh = new PricingLibrary.FinancialProducts.Share("BNP", "1");
             //var sh2 = new PricingLibrary.FinancialProducts.Share("CA", "2");
-            var opt = new PricingLibrary.FinancialProducts.VanillaCall("option", sh, date, 12);
-            var dic = new Dictionary<String, decimal>() { {"1", 38.1m }};
-            var payoff = opt.GetPayoff(dic);
-            Console.WriteLine(payoff);
+            opt = new PricingLibrary.FinancialProducts.VanillaCall("option", sh, date, 12);
+            dic = new Dictionary<String, decimal>() { {"1", 38.1m }};
         }
+        #endregion Public Constructors
+
+        #region Public Properties
+
+        public string ViewPayOff
+        {
+           
+           get { Console.WriteLine("dans get"); return opt.GetPayoff(dic).ToString(); }
+        }
+
+        #endregion Public Properties
+
     }
 }
