9588fcc1c8345a5f69f7fb31d88609c33385b86a
Etienne <etienne.lher@gmail.com>
2017-09-06 13:46:45 +0200

    Remove unused packages

--- WpfApplication1/WpfApplication1/Model/Initializer/HardCodeInitializer.cs
+++ WpfApplication1/WpfApplication1/Model/Initializer/HardCodeInitializer.cs
@@ -1,8 +1,5 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using PricingLibrary.FinancialProducts;
 
 namespace WpfApplication1.Model.Initializer
