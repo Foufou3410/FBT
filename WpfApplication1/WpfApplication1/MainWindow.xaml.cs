3e9f45ce551296e0b4674f02a5c65ccada6c3a43
ensimag <ensimag@ENSIPC432>
2017-09-07 15:30:19 +0200

    bout de window

--- WpfApplication1/WpfApplication1/MainWindow.xaml.cs
+++ WpfApplication1/WpfApplication1/MainWindow.xaml.cs
@@ -21,10 +21,44 @@ namespace FBT
     /// </summary>
     public partial class MainWindow : Window
     {
+        private List<String> viewTypesList;
+        private String startDate;
+       //private Textbox dateBox;
+
         public MainWindow()
         {
             InitializeComponent();
             this.DataContext = new MainWindowView();
+            //var dateBox = new TextBox();
+            viewTypesList = new List<string>();
+            viewTypesList.Add("Historique");
+            viewTypesList.Add("Simulées");
+
+        }
+
+
+    private void Add_Click(object sender, RoutedEventArgs e)
+    {
+        int num1 = int.Parse(dateBox.Text);
+        //Result.Text = (num1).ToString();
+        MessageBox.Show(num1.ToString());
+        // MessageBox.Show("The Result is: " + Result.Text);
+    }
+
+    public List<String> valuesType
+    {
+        get
+        {
+            return viewTypesList;
         }
     }
+
+    public String selectedValuesType { get; set; }
+
+  
+
+    //Tickerstarted => retrieve selected values 
+
+}
+
 }
--- WpfApplication1/WpfApplication1/MainWindowView.cs
+++ WpfApplication1/WpfApplication1/MainWindowView.cs
@@ -6,7 +6,16 @@ using System.Text;
 using System.Threading.Tasks;
 using AppelWRE;
 using System.Diagnostics;
+using System.Windows;
 using System.Runtime.InteropServices;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
 
 using FBT.Model.FinancialModel;
 using FBT.Model.Initializer;
@@ -15,12 +24,13 @@ namespace FBT
 {
     class MainWindowView
     {
-
+    
         #region Public Constructors
 
-
-        public MainWindowView()
+         public MainWindowView()
         {
+           
+
             //public void WREmodelingCovTest()
             //      {
             // header
@@ -33,29 +43,29 @@ namespace FBT
                                 {0.1, 0.1, 0.3}};
 
             // call WRE via computeCovarianceMatrix encapsulation
-            //WRE classWRE = new WRE();
             double[,] myCovMatrix = WRE.computeCovarianceMatrix(returns);
 
             // display result
             WRE.dispMatrix(myCovMatrix);
 
-            // ending the program            
-            Console.WriteLine("\nType enter to exit");
-            //Console.ReadKey(true);
-            // }
-
+            //Console.WriteLine();
 
 
         }
         #endregion Public Constructors
+        
 
         #region Public Properties
+
+      
 
         public string ViewPayOff
         {
            
            get {return "coucou"; }
         }
+
+    
 
         #endregion Public Properties
 
