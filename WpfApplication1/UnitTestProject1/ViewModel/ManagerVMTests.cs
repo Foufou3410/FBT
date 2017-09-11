044eccdf2a9afb2a5553b55edeea29ed386bddb5
Flo <florent.francois@grenoble-inp.org>
2017-09-11 09:41:56 +0200

    solve pb of merge

---
+++ WpfApplication1/UnitTestProject1/ViewModel/ManagerVMTests.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using FBT.ViewModel;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+namespace FBT.ViewModel.Tests
+{
+    [TestClass()]
+    public class ManagerVMTests
+    {
+        [TestMethod()]
+        public void ManagerVMTest()
+        {
+            DateTime theDate = new DateTime(1900, 1, 1);
+            ManagerVM boss = new ManagerVM(theDate, "365", "5");
+            string[] Labels = boss.Labels;
+            Assert.AreEqual(DateTime.Parse(Labels.First()), theDate);
+            Assert.AreEqual(DateTime.Parse(Labels.Last()), theDate.AddYears(1));
+            Assert.AreEqual(Labels.LongCount(), 74);
+        }
+
+        [TestMethod()]
+        public void pleaseUpdateManagerTest()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
